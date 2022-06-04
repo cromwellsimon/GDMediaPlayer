643f2cb0234d2979ee9f7b72b873e9901e8cffa1
Simon Cromwell <cromwellsimon@gmail.com>
2022-06-04 13:25:46 -0400

    Been a while since I last worked on this lol but for all intents and purposes, this project is done. There are some limitations that I can't... easily... get around lol, I'm sure I could do some more stuff if I were willing to modify the engine but then I'd have to distribute the source files to the modified engine and, at that point, there isn't even really a point in doing any of that lol. You can only seek in audio and you can only seek while the stream is paused. There is no way (again, without altering the engine itself) to be able to either seek or get the total track time of a video which is a little unfortunate but it's okay, for the scope of the project, this is the bare necessities lol

--- Godot/MediaPlayer/AudioVideoPlayer/AudioVideoControls/AudioVideoControls.gd
+++ Godot/MediaPlayer/AudioVideoPlayer/AudioVideoControls/AudioVideoControls.gd
@@ -1,13 +1,16 @@
 extends VBoxContainer
 
 export(NodePath) var PlayPause_path
-onready var play_pause_button = get_node(PlayPause_path)
+onready var play_pause_button : Button = get_node(PlayPause_path)
 
 export(NodePath) var TimeSlider_path
-onready var time_slider = get_node(TimeSlider_path)
+onready var time_slider : HSlider = get_node(TimeSlider_path)
 
 export(NodePath) var TimeElapsed_path
-onready var time_elapsed = get_node(TimeElapsed_path)
+onready var time_elapsed : Label = get_node(TimeElapsed_path)
+
+export(NodePath) var VolumeSlider_path
+onready var volume_slider : HSlider = get_node(VolumeSlider_path)
 
 signal on_play_pause_toggled(playing)
 signal on_volume_slider_changed(db)
@@ -19,21 +22,22 @@ signal on_volume_slider_changed(db)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
 	update_play_pause_button_text()
+	init_volume_slider()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func init_time_controls(max_value):
-	time_slider.max_value = max_value
+	time_slider.max_value = stepify(max_value, 0.01)
 	time_elapsed.text = "0/" + str(time_slider.max_value)
-	
+
 
 func update_time_controls(value):
-	time_slider.value = value
+	time_slider.value = stepify(value, 0.01)
 	time_elapsed.text = str(time_slider.value) + "/" + str(time_slider.max_value)
+
 
 func update_play_pause_button_text():
 	if(!play_pause_button.pressed):
@@ -41,12 +45,15 @@ func update_play_pause_button_text():
 	else:
 		play_pause_button.text = "Pause"
 
+
+func init_volume_slider():
+	volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+
+
 func _on_PlayPause_toggled(button_pressed):
-	pass # Replace with function body.
 	update_play_pause_button_text()
 	emit_signal("on_play_pause_toggled", !button_pressed)
 
 
 func _on_VolumeSlider_value_changed(value):
-	pass # Replace with function body.
 	emit_signal("on_volume_slider_changed", value)
--- Godot/MediaPlayer/AudioVideoPlayer/AudioVideoPlayer.gd
+++ Godot/MediaPlayer/AudioVideoPlayer/AudioVideoPlayer.gd
@@ -9,31 +9,27 @@ onready var audio_stream_player : AudioStreamPlayer = get_node(AudioStreamPlayer
 export(NodePath) var VideoPlayer_path
 onready var video_player : VideoPlayer = get_node(VideoPlayer_path)
 
-var volume : float
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var volume : float = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	update_volume()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	if (audio_stream_player.is_playing()):
+	if (audio_stream_player.is_playing() and !audio_stream_player.stream_paused):
 		controls.update_time_controls(audio_stream_player.get_playback_position())
-	elif (video_player.is_playing()):
+	elif (video_player.is_playing() and !video_player.paused):
 		controls.update_time_controls(video_player.get_stream_position())
+
 
 func play_audio_stream(audio_stream) -> void:
 	audio_stream_player.set_stream(audio_stream)
 	audio_stream_player.play()
 	controls.init_time_controls(audio_stream.get_length())
+
 
 func play_video_stream(video_stream) -> void:
 	video_player.set_stream(video_stream)
@@ -50,8 +46,19 @@ func update_volume() -> void:
 func _on_AudioVideoControls_on_play_pause_toggled(playing):
 	pass # Replace with function body.
 	if (audio_stream_player.is_playing()):
+		# The only way to be able to seek with this setup, since HSlider's
+		# value_changed callback function is called regardless of who called it,
+		# is to first pause the stream, set it, and then resume it.
+		# I could have the stream automatically pause any time the user left-clicks
+		# but in essence, this is how it has to be done lol
+		if (audio_stream_player.stream_paused):
+			audio_stream_player.seek(controls.time_slider.value)
 		audio_stream_player.set_stream_paused(!playing)
 	elif (video_player.is_playing()):
+		# From the docs: Note, changing this value won't have any effect as seeking
+		# is not implemented yet, except in video formats implemented by a GDNative add-on
+		#if (video_player.paused):
+		#	video_player.set_stream_position(controls.time_slider.value)
 		video_player.set_paused(!playing)
 
 
